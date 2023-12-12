799d69a493690d4f8c12640422fd710b840f77da
nnandani25 <navya.nandani@menloschool.org>
2023-12-11 17:16:19 -0800

    updated glass so there is no writing

--- glass.py
+++ glass.py
@@ -16,27 +16,22 @@ class Glass(FallingObject):
         
         # Has the objects going in random places for the first 5 seconds
         if pygame.time.get_ticks() < 5000:
-            print("RANDOM")
             self.rect.x = random.randrange(gc.WIDTH - self.rect.width)
         
         # Has the objects going to the player between 5 and 10 seconds
         elif pygame.time.get_ticks() < 10000:
-            print("PLAYER")
             self.rect.x = self.player.rect.x
 
         # After 10 seconds it goes in the direction of where the player will be at next
         elif self.player.pressed_keys["left"] == True:
-            print("LEFT")
             self.rect.x = self.player.rect.x - 30
             self.speed = 6
         
         elif self.player.pressed_keys["right"] == True:
-            print("RIGHT")
             self.speed = 6
             
             self.rect.x = self.player.rect.x + 30
         
         # Otherwise, it will go to the player
         else:
-            print("PLAYER")
             self.rect.x = self.player.rect.x
